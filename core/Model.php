2d070e2fd9f510049d0136d8f2b217e1aa1ce352
Saroar Hossain <limonhossain57@gmail.com>
2022-08-25 01:35:54 +0600

    The validation rule and error message system has changed

--- core/Model.php
+++ core/Model.php
@@ -28,38 +28,36 @@ abstract class Model
             $value = $this->{$attribute};
 
             foreach ($rules as $rule){
-                $ruleName = $rule;
-                if(is_array($ruleName)){
-                    $ruleName = $rule[0];
-                }
+
+                $ruleName = $rule['rule'];
+
                 if($ruleName === self::RULE_REQUIRED && !$value){
-                    $this->addError($attribute, self::RULE_REQUIRED);
+                    $this->addError($attribute, $rule['message']);
                 }
 
                 if($ruleName === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)){
-                    $this->addError($attribute, self::RULE_EMAIL);
+                    $this->addError($attribute, $rule['message']);
                 }
 
                 if($ruleName === self::RULE_MIN && strlen($value) < $rule['min']){
-                    $this->addError($attribute, self::RULE_MIN, $rule);
+                    $this->addError($attribute, $rule['message']);
                 }
 
                 if($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}){
-                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                    $this->addError($attribute, $rule['message']);
                 }
 
                 if($ruleName === self::RULE_UNIQUE){
                     $className = $rule['class'];
-                    $uniqueAttr = $rule['attribute'] ?? $attribute;
                     $tableName = $className::tableName();
 
-                    $statement = Application::$app->database->prepare("SELECT * FROM $tableName WHERE $uniqueAttr = :attr");
+                    $statement = Application::$app->database->prepare("SELECT * FROM $tableName WHERE $attribute = :attr");
                     $statement->bindValue(":attr", $value);
 
                     $statement->execute();
                     $record = $statement->fetchObject();
                     if($record){
-                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $attribute]);
+                        $this->addError($attribute, $rule['message']);
                     }
                 }
             }
@@ -68,24 +66,8 @@ abstract class Model
         return empty($this->errors);
     }
 
-    private function addError($attibute, $ruleName, $params = []){
-        $message = $this->errorMessages()[$ruleName];
-
-        foreach ($params as $key => $value){
-            $message = str_replace("{{$key}}", $value, $message);
-        }
-
+    private function addError($attibute, $message){
         $this->errors[$attibute] = $message;
-    }
-
-    private function errorMessages(){
-        return [
-            self::RULE_REQUIRED => 'This field is required',
-            self::RULE_EMAIL => 'This field must be valid email address',
-            self::RULE_MIN => 'Min length of this field must be {min}',
-            self::RULE_MATCH => 'This field must be the same as {match}',
-            self::RULE_UNIQUE => 'The {field} already exists in database',
-        ];
     }
 
     public function hasError($attibute){
