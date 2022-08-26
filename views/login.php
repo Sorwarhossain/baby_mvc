bb36e7259d4c72f240e1bdd97ab00a7699a035b1
Saroar Hossain <limonhossain57@gmail.com>
2022-08-26 12:21:05 +0600

    Implement Login Page

--- views/login.php
+++ views/login.php
@@ -1,26 +1,18 @@
+<?php
+/** @var $model App\Models\User */
+?>
 <section class="contact py-5">
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Login Your Account</h1>
-                <form class="row g-3" method="post" action="">
-                    <div class="form-group">
-                        <label for="subject" class="form-label">Subject</label>
-                        <input type="text" class="form-control" id="subject" name="subject">
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="body" class="form-label">Body</label>
-                        <textarea class="form-control" name="body" id="" cols="30" rows="7"></textarea>
-                    </div>
-
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary">Sign in</button>
-                    </div>
-                </form>
+                <h1>Login To Your Account</h1>
+                <?php $form = \App\Core\Forms\Form::begin('', 'post'); ?>
+                <?php echo $form->field($model, 'email', 'Email')->fieldType('email'); ?>
+                <?php echo $form->field($model, 'password', 'Password')->fieldType('password'); ?>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+                <?php echo \App\Core\Forms\Form::end(); ?>
             </div>
         </div>
     </div>
